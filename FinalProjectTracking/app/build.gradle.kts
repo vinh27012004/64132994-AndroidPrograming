406c11895f74d7aaeaf42ceb828db235e6f7e4cc
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-11-26 14:26:19 +0700

    commit

--- FinalProjectTracking/app/build.gradle.kts
+++ FinalProjectTracking/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- FinalProjectTracking/app/src/main/java/ntu/vinh/finalprojecttracking/MainActivity.java
+++ FinalProjectTracking/app/src/main/java/ntu/vinh/finalprojecttracking/MainActivity.java
@@ -12,11 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, Vinh");
-        }
+    }
 }
