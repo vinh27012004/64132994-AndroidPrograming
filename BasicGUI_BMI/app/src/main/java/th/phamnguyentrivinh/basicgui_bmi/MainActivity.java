288437a282fbabba1964e9ef380f183fd6c8eed9
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2024-12-03 15:15:23 +0700

    xml , main

--- BasicGUI_BMI/app/src/main/java/th/phamnguyentrivinh/basicgui_bmi/MainActivity.java
+++ BasicGUI_BMI/app/src/main/java/th/phamnguyentrivinh/basicgui_bmi/MainActivity.java
@@ -1,6 +1,11 @@
 package th.phamnguyentrivinh.basicgui_bmi;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtWeight, edtHeight;
+    private CheckBox checkBoxAsian;
+    private Button btnCalculate;
+    private TextView tvResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtWeight = findViewById(R.id.edtWeight);
+        checkBoxAsian = findViewById(R.id.checkBoxAsian);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        tvResult = findViewById(R.id.tvResult);
+
+
+        btnCalculate.setOnClickListener(v -> {
+
+            String strWeight = edtWeight.getText().toString();
+            String strHeight = edtHeight.getText().toString();
+
+
+            if (strWeight.isEmpty() || strHeight.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập đủ chiều cao và cân nặng", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            double weight = Double.parseDouble(strWeight);
+            double height = Double.parseDouble(strHeight);
+
+            if(weight <= 0 || height<= 0){
+                Toast.makeText(MainActivity.this, "Vui lòng nhập số lớn hơn 0", Toast.LENGTH_SHORT).show();
+                return;
+            }
         });
     }
 }
